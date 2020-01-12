a24ebe2430d981868e5d2c1d191f103f038983ca
jooprima <jooprima@gmail.com>
2020-01-12 16:51:36 +0700

    native base

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Button, Container, Text} from 'native-base';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Button>
+          <Text>Button Native Base</Text>
+        </Button>
+      </Container>
+    );
+  }
+}
